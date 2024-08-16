9235af29f51f09479b935687b1511b52cd3f1677
Deepali <dizzy250703@gmail.com>
2024-08-16 23:07:09 +0530

    -a

--- script.js
+++ script.js
@@ -1,17 +1,242 @@
-console.log("Welcome to Spotify")
-async function main(){
-    let a = await fetch("http://localhost:3000/Songs/")
-    let response = await a.text();
-    console.log(response)
-    let div = document.createElement("div")
-    div.innerHTML = response;
-    let tds = div.getElementsByTagName("td")
-    console.log(tds)
+
+
+
+
+
+/*document.addEventListener('DOMContentLoaded', () => {        //dom & location
+    // Array containing the names of the mp3 files
+    const musicFiles = [
+        'Off The Record.mp3',
+        'Bloodline.mp3',
+        'Supernatural.mp3',
+        'Side To Side.mp3',
+        'Pookie.mp3',
+        'BLUE.mp3',
+        'LAMOUR DE MA VIE.mp3',
+        'Havana.mp3',
+        'Shameless.mp3',
+        'I Dont Think That I Like Her.mp3',
+        'We Don\'t Talk Anymore.mp3',
+        'When I Get Old.mp3',
+        'Heather.mp3',
+        'double take.mp3',
+        'Diet Mountain Dew.mp3',
+        'Need To Know.mp3',
+        'Woman.mp3',
+        'You Right.mp3',
+        'Drive Thru.mp3',
+        'Kiss and Make Up.mp3',
+        'Without Me.mp3',
+        'Know Me.mp3',
+        'You & Me.mp3',
+        'Doin Time.mp3',
+        'Perfect Night.mp3',
+        'Think About Us.mp3',
+        'Wasabi.mp3',
+        'ILLELLA.mp3',
+        'Know Me Too Well.mp3',
+        'Drivers license.mp3',
+        'Happier.mp3',
+        'Love is embarrassing.mp3',
+        'At My Worst.mp3',
+        'Because i liked a boy.mp3',
+        'Dancing With Your Ghost.mp3',
+        'Older.mp3',
+        'Señorita.mp3',
+        'Snowman.mp3',
+        'Still With You.mp3',
+        'Sweet.mp3',
+        'Kill Bill.mp3',
+        'London Boy.mp3',
+        'Sweet Nothing.mp3',
+        'In Your Eyes.mp3',
+        'One Of The Girls.mp3',
+        'So Tender.mp',
+    ];
+
+    // The folder where the mp3 files are located
+    const folderPath = 'D:\\Spotify_Clone\\Songs\\'; // Note the double backslashes for Windows paths
+
+    // Get the music container element
+    const musicContainer = document.getElementById('music-container');
+
+    // Loop through the music files array and create audio elements
+    musicFiles.forEach(file => {
+        // Create an audio element
+        const audioElement = document.createElement('audio');
+        audioElement.controls = true; // Add controls to the audio element
+        audioElement.src = folderPath + file; // Set the source to the file path
+
+        // Create a label for the audio file
+        const label = document.createElement('p');
+        label.textContent = file;
+
+        // Add the label and audio element to the container
+        musicContainer.appendChild(label);
+        musicContainer.appendChild(audioElement);
+    });
+});*/
+
+
+/*// List of MP3 files (you must manually list them)           //click
+const songs = [
+    'Off The Record.mp3',
+    'Bloodline.mp3',
+    'Supernatural.mp3',
+    'Side To Side.mp3',
+    'Pookie.mp3',
+    'BLUE.mp3',
+    'LAMOUR DE MA VIE.mp3',
+    'Havana.mp3',
+    'Shameless.mp3',
+    'I Dont Think That I Like Her.mp3',
+    'We Don\'t Talk Anymore.mp3',
+    'When I Get Old.mp3',
+    'Heather.mp3',
+    'double take.mp3',
+    'Diet Mountain Dew.mp3',
+    'Need To Know.mp3',
+    'Woman.mp3',
+    'You Right.mp3',
+    'Drive Thru.mp3',
+    'Kiss and Make Up.mp3',
+    'Without Me.mp3',
+    'Know Me.mp3',
+    'You & Me.mp3',
+    'Doin Time.mp3',
+    'Perfect Night.mp3',
+    'Think About Us.mp3',
+    'Wasabi.mp3',
+    'ILLELLA.mp3',
+    'Know Me Too Well.mp3',
+    'Drivers license.mp3',
+    'Happier.mp3',
+    'Love is embarrassing.mp3',
+    'At My Worst.mp3',
+    'Because i liked a boy.mp3',
+    'Dancing With Your Ghost.mp3',
+    'Older.mp3',
+    'Señorita.mp3',
+    'Snowman.mp3',
+    'Still With You.mp3',
+    'Sweet.mp3',
+    'Kill Bill.mp3',
+    'London Boy.mp3',
+    'Sweet Nothing.mp3',
+    'In Your Eyes.mp3',
+    'One Of The Girls.mp3',
+    'So Tender.mp',
+    // Add more song paths here
+];
+
+// Get references to the audio player and the playlist
+const audioPlayer = document.getElementById('audioPlayer');
+const playlist = document.getElementById('playlist');
+
+// Function to create the playlist
+function createPlaylist() {
+    songs.forEach((song, index) => {
+        // Create a list item for each song
+        const listItem = document.createElement('li');
+        listItem.textContent = `Song ${index + 1}`;
+        
+        // When the list item is clicked, play the corresponding song
+        listItem.addEventListener('click', () => {
+            audioPlayer.src = song;
+            audioPlayer.play();
+        });
+
+        // Add the list item to the playlist
+        playlist.appendChild(listItem);
+    });
 }
-main()
 
-// let songs =[
-//     {songName: "IVE - Off The Record" , filepath: "C:\Users\HP\Documents\Web_D Projects\Songs\IVE - Off The Record.mp3" ,coverPath: "cover/1.jpg"}
-// ]
+// Initialize the playlist on page load
+window.addEventListener('load', createPlaylist);*/
 
-// let audioElement = new Audio('1.mp3');
+
+document.addEventListener('DOMContentLoaded', () => {   //dom,click
+    // List of MP3 files
+    // const songs = [
+    //     'Off The Record.mp3',
+    //     'Bloodline.mp3',
+    //     'Supernatural.mp3',
+    //     'Side To Side.mp3',
+    //     'Pookie.mp3',
+    //     'BLUE.mp3',
+    //     'LAMOUR DE MA VIE.mp3',
+    //     'Havana.mp3',
+    //     'Shameless.mp3',
+    //     'I Dont Think That I Like Her.mp3',
+    //     'We Don\'t Talk Anymore.mp3',
+    //     'When I Get Old.mp3',
+    //     'Heather.mp3',
+    //     'double take.mp3',
+    //     'Diet Mountain Dew.mp3',
+    //     'Need To Know.mp3',
+    //     'Woman.mp3',
+    //     'You Right.mp3',
+    //     'Drive Thru.mp3',
+    //     'Kiss and Make Up.mp3',
+    //     'Without Me.mp3',
+    //     'Know Me.mp3',
+    //     'You & Me.mp3',
+    //     'Doin Time.mp3',
+    //     'Perfect Night.mp3',
+    //     'Think About Us.mp3',
+    //     'Wasabi.mp3',
+    //     'ILLELLA.mp3',
+    //     'Know Me Too Well.mp3',
+    //     'Drivers license.mp3',
+    //     'Happier.mp3',
+    //     'Love is embarrassing.mp3',
+    //     'At My Worst.mp3',
+    //     'Because i liked a boy.mp3',
+    //     'Dancing With Your Ghost.mp3',
+    //     'Older.mp3',
+    //     'Señorita.mp3',
+    //     'Snowman.mp3',
+    //     'Still With You.mp3',
+    //     'Sweet.mp3',
+    //     'Kill Bill.mp3',
+    //     'London Boy.mp3',
+    //     'Sweet Nothing.mp3',
+    //     'In Your Eyes.mp3',
+    //     'One Of The Girls.mp3',
+    //     'So Tender.mp',
+        // Add more song paths here
+    //];
+    
+    const songs = [
+        'D:\Spotify_Clone\Songs',
+        // 'https://open.spotify.com/search/rockstar',
+        
+        // Add more songs
+    ];
+    console.log(songs)
+    // Get references to the audio player and the playlist
+    const audioPlayer = document.getElementById('audioPlayer');
+    const playlist = document.getElementById('playlist');
+
+    // Function to create the playlist
+    function createPlaylist() {
+        songs.forEach((song, index) => {
+            // Create a list item for each song
+            const listItem = document.createElement('li');
+            listItem.textContent = `Song ${index + 1}`;
+
+            // When the list item is clicked, play the corresponding song
+            listItem.addEventListener('click', () => {
+                audioPlayer.src = song;
+                audioPlayer.play();
+            });
+
+            // Add the list item to the playlist
+            playlist.appendChild(listItem);
+        });
+    }
+
+    // Initialize the playlist
+    createPlaylist();
+});
+
